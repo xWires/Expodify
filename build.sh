24be7b1029c5d78421546401f236ba7b23d2eaf4
xWires <xwires@tangledwires.xyz>
2025-01-04 15:32:48 +0000

    Stop building Expodify.Tests

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-dotnet publish -o bin/win-x64 -r win-x64 -f net8.0
-dotnet publish -o bin/linux-x64 -r linux-x64 -f net8.0
-dotnet publish -o bin/osx-arm64 -r osx-arm64 -f net8.0
+dotnet publish Expodify -o bin/win-x64 -r win-x64 -f net8.0
+dotnet publish Expodify -o bin/linux-x64 -r linux-x64 -f net8.0
+dotnet publish Expodify -o bin/osx-arm64 -r osx-arm64 -f net8.0
